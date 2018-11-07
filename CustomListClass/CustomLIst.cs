492d3d209ef3f6e4f684f8b5462c2daeb951ac40
b-lor <blorwi@gmail.com>
2018-11-07 15:42:32 -0600

    Added remove function and a few tests to make sure it worked as intended

--- CustomListClass/CustomLIst.cs
+++ CustomListClass/CustomLIst.cs
@@ -4,6 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// custom built list to store any data type 
+// read only count property
+// c# indexer accessible via index, cannot access an out of bounds index
+// add object to an instance of list class
+// override tostring method to convert list to string
+// overload the + operator to add 2 instances
+// overload the - operator to subtract 2 instances 
+// write details of operator overload - check userstories
+// zip 2 columns together (zipper)
+
+
 namespace CustomListClass
 {
     public class CustomList<T>
@@ -24,20 +35,37 @@ namespace CustomListClass
 
         public CustomList()
         {
-            capacity = 1;
+            capacity = 0;
             items = new T[capacity];
             count = 0;
         }
 
         public void Add(T toAdd)
         {
-            if (count >= capacity - 1)
-            {
+                if (count >= capacity)
+                {
                 increaseList(capacity);
             }
             items[count] = toAdd;
             count++;
+            capacity++;
         }
+
+        public bool Remove(T toRemove)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (items[i].Equals(toRemove))
+                {
+                    items[i] = items[i + 1];
+                    i++;
+                }
+            }
+            count--;
+            capacity--;
+            return true;
+        }
+
 
         public int increaseList(int capacity)
         {
--- CustomListClass_Tests/CustomListClass_Tests.cs
+++ CustomListClass_Tests/CustomListClass_Tests.cs
@@ -67,5 +67,79 @@ namespace CustomListClass_Tests
 
             Assert.AreEqual(expected, list.Count);
         }
+        [TestMethod]
+        public void Check_If_Capacity_Equals_To_1()
+        {
+            //Arrange
+            CustomList<int> list = new CustomList<int>();
+            int expected = 1;
+            //Act
+            list.Add(16);
+
+            Assert.AreEqual(expected, list.Capacity);
+        }
+
+        [TestMethod]
+        public void Check_If_Capacity_Equals_To_5()
+        {
+            //Arrange
+            CustomList<int> list = new CustomList<int>();
+            int expected = 5;
+            //Act
+            list.Add(16);
+            list.Add(17);
+            list.Add(18);
+            list.Add(16);
+            list.Add(19);
+
+            Assert.AreEqual(expected, list.Capacity);
+        }
+
+        [TestMethod]
+        public void Check_If_Removed_Index_Changes_to_Next_Index()
+        {
+            //Arrange
+            CustomList<int> list = new CustomList<int>();
+            int expected = 16;
+            //Act
+            list.Add(17);
+            list.Add(18);
+            list.Add(16);
+            list.Add(19);
+            list.Remove(list[1]);
+
+            Assert.AreEqual(expected, list[1]);
+        }
+        [TestMethod]
+        public void Check_If_Removed_Value_Changes_to_Next_Index()
+        {
+            //Arrange
+            CustomList<int> list = new CustomList<int>();
+            int expected = 16;
+            //Act
+            list.Add(17);
+            list.Add(18);
+            list.Add(16);
+            list.Add(19);
+            list.Remove(18);
+
+            Assert.AreEqual(expected, list[2]);
+        }
+
+        [TestMethod]
+        public void Check_If_Capacity_Changes_After_Removing_Value()
+        {
+            //Arrange
+            CustomList<int> list = new CustomList<int>();
+            int capacity = 3;
+            //Act
+            list.Add(17);
+            list.Add(18);
+            list.Add(16);
+            list.Add(19);
+            list.Remove(18);
+
+            Assert.AreEqual(capacity, list.Capacity);
+        }
     }
 }
